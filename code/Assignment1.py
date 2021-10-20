391b98e546bb73cd22a26246279253f8ed0c1fb6
Mark Bates <56939035+MarkDavidBates@users.noreply.github.com>
2021-10-20 14:30:36 +0100

    Update Assignment1.py

--- code/Assignment1.py
+++ code/Assignment1.py
@@ -31,28 +31,50 @@ def create_instance():
 					]
 				},
 			],
-			UserData="""
-                		#!/bin/bash
+			UserData="""#!/bin/bash
+				yum install httpd -y
+                		yum update httpd
+                		systemctl enable httpd
+                		systemctl start httpd
                 		echo '<html>' > index.html
                 		echo 'Private IP address: ' >> index.html
                 		curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+				echo ' | Instance ID: ' >> index.html
+                		curl http://169.254.169.254/latest/meta-data/instance-id >> index.html
+                		echo " | Mac Address: " >> index.html
+                		curl http://169.254.169.254/latest/meta-data/mac >> index.html
+                		echo " | Subnet: " >> index.html
+                		curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/02:29:96:8f:>
                 		cp index.html /var/www/html/index.html""",
 		)
+		print ("waiting for instance...")
+        	time.sleep(12)
+        	print ("almost there...")
 		new_instance[0].wait_until_running()
 		print ("instance is now up and running")
 		print (new_instance[0].id)
-		print (new_instance[0].public_ip_address)
+		new_instance[0].reload()
+		ip_address = new_instance[0].public_ip_address
+        	print (ip_address)
+        	try:
+            		webbrowser.open("http://" + ip_address)
+        	except Exception as error:
+            		print (error)
 	except Exception as error:
-		print (error)
+        print (error) #error exception to prevent code from crashing
 
 def create_bucket():
-	new_bucket = s3.create_bucket(
-		ACL="public-read-write"
-		Bucket="MarkBatesAssignment1",
-        	CreateBucketConfiguration={
-            		"LocationConstraint": "eu-west-1"
-        	},
-        	ObjectlockEnablesForBucket=True
-)
+	s3 = boto3.resource("s3")
+	try:
+		new_bucket = s3.create_bucket(
+			ACL="public-read"
+			Bucket="markbatesassignment1",
+        		CreateBucketConfiguration={
+            			"LocationConstraint": "eu-west-1"
+        		},
+		)
+	except Exception as error:
+		print(error)
 
 create_instance()
+create_bucket()
